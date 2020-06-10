ec3dfc64875bcda85b3d7c9636deb2f84e7c68ee
AnithaD <anitha.k.dharaneedharan@gmail.com>
2020-06-11 00:46:40 +0100

    Toggles checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import { TodoListItem } from "./TodoListItem";
 
-const todos: Array<Todo> = [
+const initialTodos: Array<Todo> = [
   { text: "walk the dog", complete: true },
   { text: "write the app", complete: false },
 ];
 const App: React.FC = () => {
+  const [todos, setTodods] = useState(initialTodos);
+
+  function toggleTodo (selectedTodo:Todo ) {
+    console.log("entered")
+    const newTodos = todos.map(todo => {
+      if(todo === selectedTodo) {
+        console.log({...todo})
+       return { ...todo, complete: !todo.complete}
+      }
+      return todo;
+    })
+    setTodods(newTodos);
+  }
+
   return (
     <React.Fragment>
-      <TodoListItem todo={todos[0]} />
-      <TodoListItem todo={todos[1]} />
+      <TodoListItem todo={todos[0]} toggleTodo={toggleTodo}/>
+      <TodoListItem todo={todos[1]} toggleTodo={toggleTodo}/>
     </React.Fragment>
   );
 };
